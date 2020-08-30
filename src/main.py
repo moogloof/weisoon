b1c25562b62838db50c4df9dbb7de0bf9f05a55a
Ethan Yun <ethanyun@ohs.stanford.edu>
2020-08-29 23:02:21 -0700

    Stuff

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+import logging
+
+
+# Logging basic configurations
+LEVEL = logging.DEBUG
+FORMAT = "%(asctime)s::%(levelname)s::%(message)s"
+logging.basicConfig(format=FORMAT, level=LEVEL)
+
+# Disable info and debug logging from discord
+logging.getLogger("discord").setLevel(logging.WARNING)
+
+
+# Wei Soon client
+bot = commands.Bot(command_prefix="m#")
+bot.remove_command("help")
+
+# Hiss command
+@bot.command()
+async def hiss(ctx, length: int=100):
+	hiss_str = "hi" + "s"*min(12000, length)
+
+	# Post hiss string in chunks
+	while hiss_str:
+		await ctx.send("```\n" + hiss_str[:1992] + "\n```")
+		hiss_str = hiss_str[1992:]
+
+# Hiss command error
+@hiss.error
+async def hiss_error(ctx, error):
+	if isinstance(error, commands.UserInputError):
+		# Send correct usage of command
+		await ctx.send("```\nUsage: m!hiss <int|NONE>\n```")
+
+
+# Help command
+@bot.command()
+async def help(ctx):
+	author = ctx.message.author
+
+	embed = discord.Embed(colour=discord.Colour.orange())
+
+	# Help command
+	embed.set_author(name="Help")
+	embed.add_field(name="m#hiss", value="Returns hiss", inline=False)
+
+	await author.send(embed=embed)
+
+
+bot.run("NTA1MjQxODc2NTU5OTUzOTMz.W9KdKg.jsHtzR0biOC9UfoIiYPTS2IIGro")
+
